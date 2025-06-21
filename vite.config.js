1c057e239a6a317bc810330fd057c3e9ec78dd9d
ThroughMaze <amr.267.1990@gmail.com>
2025-06-21 14:53:06 +0500

    Initial commit: Add MobileBooster redesign files

---
+++ vite.config.js
@@ -0,0 +1,46 @@
+import {resolve} from 'path'
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+    root: './',
+    build: {
+        outDir: "./dist",
+        emptyOutDir: true,
+        define: {
+            'require': 'window.require',
+        },
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/js/[name].js",
+                chunkFileNames: "assets/js/[name].js",
+                assetFileNames: ({name}) => {
+                    if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')) {
+                        return 'assets/images/[name][extname]';
+                    }
+                    if (/\.css$/.test(name ?? '')) {
+                        return 'assets/css/[name][extname]';
+                    }
+                    if (/\.mp4$/.test(name ?? '')) {
+                        return 'videos/[name][extname]';
+                    }
+                    if (/\.ttf$/.test(name ?? '')) {
+                        return 'assets/fonts/[name][extname]';
+                    }
+                    if (/\.ttf$/.test(name ?? '')) {
+                        return 'fonts/[name][extname]';
+                    }
+
+                    // default value
+                    // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+                    return '[name][extname]';
+                }
+            },
+            input: {
+                main: resolve('', 'index.html'),
+                blog: resolve('', 'blog.html'),
+                article: resolve('', 'article.html'),
+                product: resolve('', 'product.html')
+            },
+        }
+    },
+});
